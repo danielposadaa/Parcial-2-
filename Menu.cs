15655b9916964154d1ab5eb32f07f7e8746b5bda
HenderWolf <snowhemmo1996@gmail.com>
2021-10-24 22:14:50 -0500

    Arreglo de la línea 202
    Se me había olvidado agregar un +

--- Menu.cs
+++ Menu.cs
@@ -199,7 +199,7 @@ namespace Parcial_2
 
                     case 6:
                         Console.WriteLine("\n" +
-                            "\n Este trabajo fue realizado por: "
+                            "\n Este trabajo fue realizado por: " +
                             "\n 1. Daniel Santiago Posada Arana" +
                             "\n 2.  Juan Esteban Suárez Restrepo" +
                             "\n 3.- Sara Marcela Posada Rendón \n"); 
